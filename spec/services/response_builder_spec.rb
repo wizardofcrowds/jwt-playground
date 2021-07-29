251d35f8a856bc32e99deebca988c87bb52cc359
Koichi Hirano <internalist@gmail.com>
2021-07-29 10:14:06 -0400

    Bug in the cache handling. This is a bug repro spec

--- spec/services/response_builder_spec.rb
+++ spec/services/response_builder_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe ResponseBuilder do
   end
 
   describe '#build' do
-    context 'with the same search cached in Search model' do
+    context 'with the same search cached in Search model that was created within the last 24 hours' do
       it 'should use cached response from Search model' do
         venue_search = instance_double(FoursquareClient::VenueSearch)
         allow(FoursquareClient::VenueSearch).to receive(:new).and_return(venue_search)
@@ -23,8 +23,30 @@ RSpec.describe ResponseBuilder do
         allow(venue_search).to receive(:slug).and_return(slug)
         search = create(:search, slug: slug, upstream_response: JSON.parse(load_mock_data('venue_search_response.txt')))
 
+        expect(venue_search).not_to receive(:execute)
+
         builder = ResponseBuilder.new(category_id: nil, keyword: 'Totto', near: 'New York, NY', sort: 'distance')
         expect(builder.build).to eq({ venues: eval(load_mock_data('venues.txt')) })
+      end
+    end
+
+    context 'with the same search cached in Search model that was updated before 24 hours ago' do
+      it 'should use cached response from Search model' do
+        venue_search = instance_double(FoursquareClient::VenueSearch)
+        allow(FoursquareClient::VenueSearch).to receive(:new).and_return(venue_search)
+        slug = 'hogehogehoge'
+        allow(venue_search).to receive(:slug).and_return(slug)
+        search = create(:search,
+                        slug: slug,
+                        upstream_response: JSON.parse(load_mock_data('venue_search_response.txt')),
+                        created_at: 3.days.ago,
+                        updated_at: 2.days.ago)
+
+        expect(venue_search).to receive(:execute).and_return(JSON.parse(load_mock_data('venue_search_response2.txt')))
+
+        builder = ResponseBuilder.new(category_id: nil, keyword: 'Totto', near: 'New York, NY', sort: 'distance')
+        expect(builder.build).to eq({ venues: eval(load_mock_data('venues.txt')) })
+        expect(search.reload.updated_at).to be_within(2).of(Time.now)
       end
     end
 
